e40ee9fd07789daca9ed0a4bda9a57301d224f8e
Lia Tazetdinova <you@example.com>
2024-03-23 23:43:08 +0300

    add avatar update router

--- db/models/user.js
+++ db/models/user.js
@@ -20,6 +20,7 @@ module.exports = (sequelize, DataTypes) => {
       password: DataTypes.STRING,
       role: DataTypes.INTEGER,
       status: DataTypes.STRING,
+      avatar: DataTypes.TEXT,
     },
     {
       sequelize,
--- src/routes/api/user.route.js
+++ src/routes/api/user.route.js
@@ -1,4 +1,6 @@
 const UserAPIRouter = require('express').Router()
+const { User } = require('../../../db/models')
+const fileMiddleware = require('../../../middleware/file')
 
 UserAPIRouter.get('/user', (req, res) => {
   const { user } = req
@@ -10,4 +12,21 @@ UserAPIRouter.get('/user', (req, res) => {
   }
 })
 
-export default UserAPIRouter
+UserAPIRouter.put('/user/:id', fileMiddleware.single('avatar'), async (req, res) => {
+  try {
+    const { user } = req
+    if (req.file) {
+      const avatarFileName = req.file.filename
+      await User.update({ avatar: avatarFileName }, { where: { id: user.id } })
+      const userUpdate = await User.findOne({ where: { id: user.id } })
+      res.status(200).json({ userUpdate })
+    } else {
+      res.status(401).json({ error: 'Error: No avatar file uploaded' })
+    }
+  } catch (error) {
+    console.error('Error updating user data:', error)
+    res.status(500).json({ error: 'Server error' })
+  }
+})
+
+module.exports = UserAPIRouter
