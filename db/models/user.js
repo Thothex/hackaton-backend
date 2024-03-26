760f688694ebde297afd044f984c8f4c97301a49
test <alinaluzanova@gmail.com>
2024-03-26 21:21:51 +0300

    statistics for profile

--- db/models/user.js
+++ db/models/user.js
@@ -7,10 +7,11 @@ module.exports = (sequelize, DataTypes) => {
      * This method is not a part of Sequelize lifecycle.
      * The `models/index` file will call this method automatically.
      */
-    static associate({ Team, Task, Organizations }) {
+    static associate({ Team, Task, Organizations,Achievement }) {
       User.belongsToMany(Team, { through: 'TeamUsers', foreignKey: 'user_id', otherKey: 'team_id' })
       User.belongsToMany(Task, { through: 'TeamAnswer', foreignKey: 'user_id', otherKey: 'task_id' })
       User.belongsToMany(Organizations, { through: 'UserOrganizations', foreignKey: 'userId', as: 'organizations' })
+      User.belongsToMany(Achievement, {through:'UserAchievements', foreignKey:'user_id', otherKey:'achievement_id'})
     }
   }
 
@@ -26,6 +27,7 @@ module.exports = (sequelize, DataTypes) => {
       role: DataTypes.INTEGER,
       status: DataTypes.STRING,
       avatar: DataTypes.TEXT,
+      score: DataTypes.INTEGER
     },
     {
       sequelize,
